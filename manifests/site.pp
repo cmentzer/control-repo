6f4d87a1993b1838aac5586a5306fb61606a9d4a
root <root@master.puppet.lan>
2016-07-20 17:30:42 +0000

    stuff

--- manifests/site.pp
+++ manifests/site.pp
@@ -26,7 +26,7 @@ File { backup => false }
 # specified in the console for that node.
 
 node 'pdx-agent-1663.puppet.lan' { 
-  contain site::role::webserver
+  include role::webserver
 }
 
 node default {
