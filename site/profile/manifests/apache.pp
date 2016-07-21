4eb70a20e9c72d72c1104a4809b17e780f2e63f5
root <root@master.puppet.lan>
2016-07-21 21:30:35 +0000

    asdf

--- site/profile/manifests/apache.pp
+++ site/profile/manifests/apache.pp
@@ -2,9 +2,11 @@ class profile::apache {
 
   # profile for the apache class
 
-  class { 'apache': }
+  class { 'apache':
+    server_signature => 'off'
+  }
   include apache::mod::alias
-  include apache::mod::perl
+  #include apache::mod::perl
   include apache::mod::php
   include apache::mod::rewrite
   include apache::mod::status
