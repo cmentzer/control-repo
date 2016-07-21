02fefc462d08fb9cd09b322eb05722d72db7a3ff
root <root@master.puppet.lan>
2016-07-21 21:43:37 +0000

    stuff

--- site/profile/manifests/swap_file.pp
+++ site/profile/manifests/swap_file.pp
@@ -1,14 +1,9 @@
 class profile::swap_file {
 
   # profile for the swap_file class
-
-  class { 'swap_file':
-    'files' => {
-      'swap1' => {
-        ensure       => present,
-        swapfile     => '/mnt/swap.1',
-        swapfilesize => '2 GB',
-      }
-    }
+  swap_file::files { 'file1':
+    ensure       => present,
+    swapfile     => '/mnt/swap.1',
+    swapfilesize => '2 GB',
   } 
 }
