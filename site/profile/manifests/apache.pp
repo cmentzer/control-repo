dab8811ed4eb6722394e6f4d7a76608e3db08949
root <root@master.puppet.lan>
2016-07-21 21:27:30 +0000

    sads

--- site/profile/manifests/apache.pp
+++ site/profile/manifests/apache.pp
@@ -2,10 +2,10 @@ class profile::apache {
 
   # profile for the apache class
 
-  class { 'apache':}
-  class { 'apache::mod::alias': }
-  class { 'apache::mod::perl': }
-  class { 'apache::mod::php': }
-  class { 'apache::mod::rewrite': }
-  class { 'apache::mod::status': } 
+  class { 'apache': }
+  include apache::mod::alias
+  include apache::mod::perl
+  include apache::mod::php
+  include apache::mod::rewrite
+  include apache::mod::status
 }
